2e97989c07cc9cad7048455624b14da8cad01071
nemundo <urs.lang@gmail.com>
2021-02-08 18:00:32 +0100

    .

--- src/Application/AbrechnungApplication.php
+++ src/Application/AbrechnungApplication.php
@@ -4,6 +4,8 @@ namespace Nemundo\Abrechnung\Application;
 
 
 use Nemundo\Abrechnung\Data\AbrechnungModelCollection;
+use Nemundo\Abrechnung\Install\AbrechnungInstall;
+use Nemundo\Abrechnung\Site\AbrechnungSite;
 use Nemundo\App\Application\Type\AbstractApplication;
 
 class AbrechnungApplication extends AbstractApplication
@@ -13,7 +15,9 @@ class AbrechnungApplication extends AbstractApplication
     {
         $this->application = 'Abrechnung';
         $this->applicationId = 'f3b39def-a194-48c6-b1c9-de92867540d6';
-        $this->modelCollectionClass=AbrechnungModelCollection::class;
+        $this->modelCollectionClass = AbrechnungModelCollection::class;
+        $this->installClass = AbrechnungInstall::class;
+        $this->siteClass = AbrechnungSite::class;
     }
 
 }
